29c3927c7e6265b3f7ebda503103d56a5a5559ee
Rob Higgins <higginsrob@gmail.com>
2017-11-03 05:17:50 -0700

    1.0.1

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ var uuid = require('uuid/v4');
 
 module.exports = function Pmessage(props) {
 
-    var scope = this;
-
-    scope.state = Object.assign({
+    const scope = this;
+    const state = scope.state = Object.assign({
         target: window.parent,
         secure: false,
-        timeout: 1000,
+        timeout: 3000,
         timeouts: {},
         origin: '*'
     }, props);
@@ -17,7 +16,6 @@ module.exports = function Pmessage(props) {
     window.addEventListener('message', e => scope.handleMessage(e));
 
     scope.handleMessage = function(e) {
-        var state = scope.state;
         var allowed = (state.secure && e.origin === window.location.origin)
             || (!state.secure && (
                 state.origin === e.origin || state.origin === '*'
@@ -28,8 +26,7 @@ module.exports = function Pmessage(props) {
         Object.keys(scope.listeners).forEach(key => {
             if (e.data && e.data.type === key && e.data.uid && scope.listeners[key].length) {
                 scope.listeners[key].forEach(listener => {
-                    var payload = listener(e.data.payload);
-                    scope.emit(e.data.uid, payload, e.data.uid);
+                    scope.emit(e.data.uid, listener(e.data.payload), e.data.uid, e.data.id);
                 });
             }
         });
@@ -52,54 +49,39 @@ module.exports = function Pmessage(props) {
 
     scope.once = function(event, fn) {
         var wrapFn = () => {
-            scope.off(event, wrapFn);
             fn(event);
+            scope.off(event);
         };
         scope.on(event, wrapFn);
     };
 
-    scope.off = function(event, fn) {
-        var listeners = scope.listeners[event];
-        if (listeners) {
-            listeners.forEach(listener => {
-                console.log(listener.toString());
-                console.log(fn.toString());
-                console.log(listener.toString() === fn.toString());
-            });
-            //var index = listeners.indexOf(fn);
-            //console.log(111, index);
-            //if (index > -1) {
-                //listeners.splice(index, 1);
-                //if (!listeners.length) {
-                    //delete scope.listeners[event];
-                //}
-            //}
-        }
+    scope.off = function(event) {
+        delete scope.listeners[event];
     };
 
     scope.send = function(type, payload) {
         return new Promise((respond, reject) => {
             var uid = uuid();
-            scope.on(uid, payload => {
-                window.clearTimeout(scope.state.timeouts[uid]);
+            scope.once(uid, payload => {
+                window.clearTimeout(state.timeouts[uid]);
                 respond(payload);
             });
-            scope.state.timeouts[uid] = window.setTimeout(() => {
+            state.timeouts[uid] = window.setTimeout(() => {
                 reject('PM Timeout Error: type: ' + type + ', uid: ' + uid);
-            }, scope.state.timeout);
+            }, state.timeout);
             scope.emit(type, payload, uid);
         });
     };
 
     scope.emit = function(type, payload, uid) {
-        if (!scope.state.target) {
+        if (!state.target) {
             throw new Error('target window not set');
         }
-        scope.state.target.postMessage(
-            {type, payload, uid},
-            scope.state.secure
+        state.target.postMessage(
+            {type, payload, uid, id: state.id},
+            state.secure
             ? window.location.origin
-            : scope.state.origin
+            : state.origin
         );
     };
 
